050bd0d1879bb28b1db00d4737c56062b0706f79
niiihiihii <letsfiretogether@icloud.com>
2023-03-18 00:11:35 +0900

    update app.js index.html

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+document.documentElement.addEventListener('touchstart', function (event) {
+    if (event.touches.length > 1) {
+         event.preventDefault(); 
+       } 
+   }, false);
+
+let lastTouchEnd = 0; 
+
+document.documentElement.addEventListener('touchend', function (event) {
+    let now = (new Date()).getTime();
+    if (now - lastTouchEnd <= 300) {
+         event.preventDefault(); 
+       } lastTouchEnd = now; 
+   }, false);
+
 const cards = document.querySelectorAll(".card");
 
 const count = [];
