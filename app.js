cb23598732bf29fce0fb59784082747f8c61738e
niiihiihii <niihiihii@naver.com>
2023-03-30 17:51:36 +0900

    cleaning code

--- app.js
+++ app.js
@@ -316,111 +316,59 @@ idolizedSwitch.addEventListener("click", idolizedOnOff);
 
 
 /* Sort by Group ON/OFF Switch */
-const fes_icon = document.querySelectorAll(".fes-icon")
-const party_icon = document.querySelectorAll(".party-icon")
-const normal_icon = document.querySelectorAll(".normal-icon")
-
-function sortByGroupOn() {
-    const fes_order = ["13", "15", "10", "3", "16",
-                        "19", "20", "23", "25", "18",
-                        "22", "24", "11", "26", "17",
-                        "2", "4", "12", "9", "14",
-                        "1", "5", "6", "8", "21",
-                        "27", "7", "28",
-
-                        "32", "36", "38", "53", "45",
-                        "46", "42", "58", "47", "52",
-                        "50", "35", "51", "54", "33",
-                        "57", "56", "43", "39", "34",
-                        "41", "44", "40", "31", "48",
-                        "49", "37", "55",
-
-                        "89", "66", "69", "62", "75",
-                        "68", "83", "77", "90", "63",
-                        "86", "87", "80", "81", "71",
-                        "70", "64", "67", "72", "84",
-                        "88", "85", "82", "79", "61",
-                        "78", "74", "76", "73", "65"];
-
-    const party_order = ["17", "14", "1", "18", "28",
-                        "15", "19", "3", "20", "6",
-                        "24", "2", "27", "30", "11",
-                        "12", "25", "5", "22", "29",
-                        "13", "16", "10", "9", "8",
-                        "23", "4"];
-
-    const normal_order = ["3", "20", "14", "16"];
-
-    for (let i = 0; i < fes_cards.length; i++) {
-        fes_cards[i].style.order = fes_order[i];
-    }
-    for (let i = 0; i < party_cards.length; i++) {
-        party_cards[i].style.order = party_order[i];
-    }
-    for (let i = 0; i < normal_cards.length; i++) {
-        normal_cards[i].style.order = normal_order[i];
-    }
-
-    const fes_icon_order = ["29", "30", "59", "60"];
-    const party_icon_order = ["7", "21", "26"]
-    const normal_icon_order = ["1", "2", "4", "5",
-                                "6", "7", "8", "9", "10",
-                                "11", "12", "13", "15",
-                                "17", "18", "19",
-                                "21", "22", "23", "24", "25",
-                                "26", "27", "28", "29", "30"]
-
-    for (let i = 0; i < fes_icon.length; i++) {
-        fes_icon[i].style.order = fes_icon_order[i];
-        fes_icon[i].classList.remove("displayNone");
-    }
-    for (let i = 0; i < party_icon.length; i++) {
-        party_icon[i].style.order = party_icon_order[i];
-        party_icon[i].classList.remove("displayNone");
-    }
-    for (let i = 0; i < normal_icon.length; i++) {
-        normal_icon[i].style.order = normal_icon_order[i];
-        normal_icon[i].classList.remove("displayNone");
-    }
-    sortByGroupSwitch.innerText = "Sort by Group ON";
-}
-
-function sortByGroupOff() {
-    for (let i = 0; i < fes_cards.length; i++) {
-        fes_cards[i].style.order = "";
-    }
-    for (let i = 0; i < party_cards.length; i++) {
-        party_cards[i].style.order = "";
-    }
-    for (let i = 0; i < normal_cards.length; i++) {
-        normal_cards[i].style.order = "";
-    }
-    for (let i = 0; i < fes_icon.length; i++) {
-        fes_icon[i].style.order = "";
-        fes_icon[i].classList.add("displayNone");
-    }
-    for (let i = 0; i < party_icon.length; i++) {
-        party_icon[i].style.order = "";
-        party_icon[i].classList.add("displayNone");
-    }
-    for (let i = 0; i < normal_icon.length; i++) {
-        normal_icon[i].style.order = "";
-        normal_icon[i].classList.add("displayNone");
-    }
-    sortByGroupSwitch.innerText = "Sort by Group OFF";
-}
-
 function sortByGroupOnOff() {
-    sortByGroup_switch_click_count++;
-    if (sortByGroup_switch_click_count % 2 === 0) {
-        sortByGroupOff();
+    const orderList = ["honoka",
+                        "eli",
+                        "kotori",
+                        "chika",
+                        "riko",
+                        "kanan",
+                        "ayumu",
+                        "kasumi",
+                        "shizuku",
+                        "karin",
+                        "umi",
+                        "rin",
+                        "maki",
+                        "dia",
+                        "you",
+                        "yoshiko",
+                        "ai",
+                        "kanata",
+                        "setsuna",
+                        "emma",
+                        "nozomi",
+                        "hanayo",
+                        "nico",
+                        "hanamaru",
+                        "mari",
+                        "ruby",
+                        "rina",
+                        "shioriko",
+                        "mia",
+                        "lanzhu"];
+    
+    for (let i = 0; i < orderList.length; i++) {
+        let schoolidol = orderList[i];
+        let idol_cards = document.querySelectorAll("." + schoolidol);
+        for (let i = 0; i < idol_cards.length; i++) {
+            idol_cards[i].classList.toggle(schoolidol + "__order");
+        }
+    }
+
+    const icon = document.querySelectorAll(".icon");
+    for (let i = 0; i < icon.length; i++) {
+        icon[i].classList.toggle("displayNone");
+    }
+
+    if (fes_cards[0].classList.contains("maki__order")) {
+        sortByGroupSwitch.innerText = "Sort by Group ON";
     } else {
-        sortByGroupOn();
+        sortByGroupSwitch.innerText = "Sort by Group OFF";
     }
 }
 
 const sortByGroupSwitch = document.querySelector(".sortByGroupSwitch");
-let sortByGroup_switch_click_count = 0;
 
 sortByGroupSwitch.addEventListener("click", sortByGroupOnOff);
 /* Sort by Group ON/OFF Switch */
