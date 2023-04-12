b07b47837d5bcaa9d717f7b67b8b1a170cde4dbc
niiihiihii <niihiihii@naver.com>
2023-04-13 08:06:23 +0900

    add animation

--- app.js
+++ app.js
@@ -319,18 +319,24 @@ function changeCardStyle() {
         for (let i = 0; i < signature.length; i++) {
             signature[i].classList.remove("displayNone");
             rainbow[i].classList.add("displayNone");
+            signature[i].classList.add("animation");
+            rainbow[i].classList.remove("animation");
         }
         changeCardStyleButton.innerText = "Signature";
     } else if (changeCardStyleButton_click_count % 4 === 1) {
         for (let i = 0; i < signature.length; i++) {
             signature[i].classList.add("displayNone");
             rainbow[i].classList.remove("displayNone");
+            signature[i].classList.remove("animation");
+            rainbow[i].classList.add("animation");
         }
         changeCardStyleButton.innerText = "Rainbow";
     } else if (changeCardStyleButton_click_count % 4 === 2) {
         for (let i = 0; i < signature.length; i++) {
             signature[i].classList.remove("displayNone");
             rainbow[i].classList.remove("displayNone");
+            signature[i].classList.remove("animation");
+            rainbow[i].classList.add("animation");
         }
         changeCardStyleButton.innerText = "Signature + Rainbow";
     } else {
